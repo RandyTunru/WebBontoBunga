19df3512db21887a184609f61a393d7dbc4a4a72
RandyTunru <randytunru@gmail.com>
2024-08-02 08:22:37 +0800

    initial commit

--- src/components/OrganisasiCard/index.js
+++ src/components/OrganisasiCard/index.js
@@ -6,11 +6,8 @@ function OrganisasiCard(item) {
   return (
     <div
     key={item.id}
-    className="flex flex-col border text-center items-center w-[274px] h-[300px] p-5 rounded-3xl shadow-lg shadow-white-hover bg-white"
+    className="flex flex-col border text-center items-center w-[274px] h-[250px] p-5 rounded-3xl shadow-lg shadow-white-hover bg-white"
   >
-    <h2 className="flex items-center font-bold text-blue-dark text-md h-16">
-      {item.title}
-    </h2>
     <Image
       className="w-24 h-24 my-4 rounded-full "
       src={item.image != "" ? item.image : '/icons/profile.svg'}
